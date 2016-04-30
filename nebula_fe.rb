0cf18ec640b881b9022a7aa6c88f8a398b05df7c
Elio Grieco <grieco@egx.com>
2016-04-30 14:18:38 -0700

    Add LaterDude to render calendar
    https://github.com/clemens/later_dude

--- nebula_fe.rb
+++ nebula_fe.rb
@@ -1,10 +1,21 @@
 require 'pathname'
 require 'sinatra'
 
+require 'rubygems'
+require 'action_view'
+require 'active_support/core_ext'
+require 'later_dude'
+
 raise 'No conversation directory specified.' if ARGV.length < 1
 
 conversation_root_dir = Pathname.new(ARGV.first)
 
 get '/' do
   conversation_root_dir.entries.join("<br>\n")
+end
+
+get '/calendar' do
+  current_time = Time.now
+
+  LaterDude::Calendar.new(current_time.year, current_time.month).to_html
 end
