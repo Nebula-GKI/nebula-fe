356b56d6fcefc16208aab8e7f58ad669123e84bf
Elio Grieco <grieco@egx.com>
2016-05-01 15:45:25 -0700

    Add event creation page

--- nebula_fe.rb
+++ nebula_fe.rb
@@ -5,6 +5,7 @@ require 'rubygems'
 require 'action_view'
 require 'active_support/core_ext'
 require 'later_dude'
+require 'sinatra/form_helpers'
 
 raise 'No conversation directory specified.' if ARGV.length < 1
 
@@ -18,4 +19,12 @@ get '/calendar' do
   current_time = Time.now
 
   LaterDude::Calendar.new(current_time.year, current_time.month).to_html
+end
+
+get '/event' do
+  haml :event
+end
+
+post '/event' do
+  params.inspect
 end
