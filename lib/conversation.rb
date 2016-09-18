04a0ec144bc213bf74ce89bd262b634ab893e1c7
Elio Grieco <grieco@egx.com>
2016-09-17 21:15:36 -0700

    Add a temporary username placeholder

--- lib/conversation.rb
+++ lib/conversation.rb
@@ -2,10 +2,12 @@ require 'pathname'
 
 module Nebula
   class Conversation
-    attr_reader :root_path
+    attr_reader :root_path, :user_name
 
     def initialize(root_path)
       @root_path = Pathname.new(root_path)
+      # :TODO: read actual username from local node
+      @user_name = 'USERNAME'
     end
   end
 end
