602497959eb77db3753842604bee103b18e35065
Elio Grieco <grieco@egx.com>
2016-09-18 16:43:00 -0700

    Add Utility::parse_chrono_file_name

--- lib/utility.rb
+++ lib/utility.rb
@@ -10,5 +10,17 @@ module Nebula
       # :TODO: need to sanitize the output of 'node_user_name' so it is suitable for a file name
       [[chrono_file_dt(time), user_name].join('-'), suffix].join('.')
     end
+
+    def parse_chrono_file_name(str)
+      basename, _, extension = str.rpartition('.')
+      date_str, _, name = basename.rpartition('-')
+      date = DateTime.strptime(date_str, CHRONO_FILE_DATE_FORMAT)
+
+      {
+        date: date,
+        name: name,
+        extension: extension
+      }
+    end
   end
 end
