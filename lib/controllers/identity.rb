0f39b65ef577f4b6b0e565d160d1a8f6d0bfa035
Elio Grieco <grieco@egx.com>
2016-09-21 05:51:45 -0700

    Name is now written to the identity file

--- lib/controllers/identity.rb
+++ lib/controllers/identity.rb
@@ -2,4 +2,8 @@ class NebulaFe < Sinatra::Base
   get '/identity' do
     haml :identity
   end
+
+  post '/identity' do
+    $identity.name = params[:identity][:name]
+  end
 end
--- lib/models/identity.rb
+++ lib/models/identity.rb
@@ -1,13 +1,23 @@
+require 'yaml'
+
 module Nebula
   class Identity
     class Error < Nebula::Error; end
     class BlankRootPath < Error; end
     class RootPathDoesNotExist < Error; end
 
+    attr_reader :root_path
+
     def initialize(root_path)
       raise BlankRootPath if root_path.to_s.blank?
       @root_path = Pathname.new(root_path).expand_path
       raise RootPathDoesNotExist, @root_path unless @root_path.exist?
+    end
+
+    def name=(name)
+      (root_path + 'identity.yaml').open('w') do |f|
+        f.write YAML.dump({name: name})
+      end
     end
   end
 end
