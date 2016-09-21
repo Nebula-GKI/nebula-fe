879e02f157b8f236ecc97b6020b68ccd1fcffbee
Elio Grieco <grieco@egx.com>
2016-09-21 01:21:33 -0700

    Add message controller

--- nebula_fe.rb
+++ nebula_fe.rb
@@ -46,27 +46,6 @@ get '/' do
   haml :main
 end
 
-get '/messages' do
-  begin
-    messages = Nebula::Message.list(conversation)
-  rescue Errno::ENOENT
-    messages = []
-  end
-  
-  json messages
-end
-
-post '/message' do
-  msg = MultiJson.load(request.body, symbolize_keys: true)
-  STDERR.puts msg.inspect
-  Nebula::Message.new(conversation, msg[:message]).save
-
-  messages = Nebula::Message.list(conversation)
-
-  status 201
-  MultiJson.dump(messages)
-end
-
 get '/calendar' do
   current_time = Time.now
 
